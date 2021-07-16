c1a1043b528113539ce4c8fcf98f7d5b115c23fd
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-07-16 17:13:41 -0400

    Angular learning class1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {SeverComponent} from './server/server.component';
+import { CardsComponent } from './cards/cards.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeverComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent {
+
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+}
